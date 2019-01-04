71cc43bb58d2906691ae11a264404c98bc3467fc
Jeff Benton <sikletz@gmail.com>
2019-01-04 01:07:37 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,7 +16,7 @@ def assign_rooms(speakers)
   assigned
 end
 
-def printer
-  puts batch_badge_creator(SPEAKERS)
-  puts assign_rooms(SPEAKERS)
+def printer(attendees)
+  puts batch_badge_creator(attendees)
+  puts assign_rooms(attendees)
 end
